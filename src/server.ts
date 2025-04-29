32d0d0f0dbf6f897e61e94c85a906920047caefe
Izabela <izabela.m.alfenas@gmail.com>
2025-04-29 16:28:05 -0300

    initial project setup

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,34 @@
-import { fastify } from 'fastify';
-import { fastifyCors } from '@fastify/cors';
+import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
+import {
+  validatorCompiler,
+  serializerCompiler,
+} from "fastify-type-provider-zod";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
 
 const app = fastify();
 
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifyCors, { origin: "*" });
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Fastify + Zod PoC",
+      version: "1.0.0",
+    },
+  },
+});
+
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
+
+app.get('/', () => { return 'Hello, World!' });
+
 app.listen({ port: 3333 }).then(() => {
-  console.log('Server is running on port 3333');
+  console.log("Server is running on port 3333");
 });
